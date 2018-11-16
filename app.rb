f1b574258c02f55c5156da9fd71db671b11af4b1
Travis T <travistokuyama@gmail.com>
2018-11-16 05:27:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,6 +23,5 @@ class App < Sinatra::Base
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     phrase = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
-    phrase
   end
 end
