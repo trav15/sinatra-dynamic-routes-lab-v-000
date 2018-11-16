6d761326e62f8577ca35754f2f1af2d8ccfba32c
Travis T <travistokuyama@gmail.com>
2018-11-16 05:33:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
+    operation = params[:operation]
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     if operation == "plus"
