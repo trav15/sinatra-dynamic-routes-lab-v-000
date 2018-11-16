d1dffc78075b1429381588f9e201485069ab3c46
Travis T <travistokuyama@gmail.com>
2018-11-16 05:30:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,4 +24,11 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     phrase = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    if operation == "plus"
+      return "#{params[:number1].to_i + params[:number2.to_i]}"
+      
+  end
 end
